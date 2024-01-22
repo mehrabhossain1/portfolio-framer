a0e9e1c88a8eb3b10881a56b6c471865e5a95d13
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-22 13:10:15 +0600

    nav and banner

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../../shared/Navbar';
+import Banner from './Banner';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
